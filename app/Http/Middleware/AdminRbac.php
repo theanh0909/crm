1436891b9a81b7a998ab3ca45d3f214db4cc331f
@theanhgxd <theanh@gxd.vn>
2021-03-07 17:50:04 +0700

    Sua trang chu

--- app/Http/Middleware/AdminRbac.php
+++ app/Http/Middleware/AdminRbac.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use App\Models\License;
 
 class AdminRbac
 {
@@ -12,7 +13,7 @@ class AdminRbac
     public function handle($request, Closure $next, $permission)
     {
         if ($permission == 'delete-key') {
-            $license_id = $request->license_id;
+            $license_id = $request->id;
             $license = License::find($license_id);
 
             if (can($permission) || $license->id_user == auth()->user()->id) {
