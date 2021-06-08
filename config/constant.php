8c1d62a03aef1474782e6356991cbe2a486ad999
@theanhgxd <theanh@gxd.vn>
2021-06-08 19:50:20 +0700

    Sua loi API check Hardward ID

--- config/constant.php
+++ config/constant.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'Khách hàng tiềm năng1',
-    'Khách hàng tiềm năng2',
-    'Khách hàng tiềm năng3',
-    'Khách hàng tiềm năng4',
-    'Khách hàng tiềm năng5',
-    'Khách hàng tiềm năng6',
-    'Khách hàng tiềm năng7',
+    'Khách hàng tiềm năng',
+    'Khách hàng quan tâm',
+    'Khách hàng mua hàng',
+    'Khách hàng khách hàng mua nhiều lần',
+    'Khách hàng trung thành',
+    'Khách hàng rất trung thành',
+    'Khách hàng cuồng nhiệt',
 ];
